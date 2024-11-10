3a4ce63c73cc1d268eca21eb39233229cafce474
am-cid <mcidantonio@gmail.com>
2024-11-10 23:42:07 +0800

    chore(process): process module init
    process contains the data manipulation and processing functionalities.
    eg: data cleaning

---
+++ da-project-backend/process/read.py
@@ -0,0 +1,10 @@
+import io
+from typing import Any
+
+import polars as pl
+
+
+def read_csv(file_contents: bytes) -> list[dict[str, Any]]:
+    "reads csv from uploaded file and returns list of rows as dicts"
+    df = pl.read_csv(io.BytesIO(file_contents))
+    return df.to_dicts()
