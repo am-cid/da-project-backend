a390c7719d543654d1e5f51d467c477e92af65c1
am-cid <mcidantonio@gmail.com>
2024-11-15 22:28:57 +0800

    feat(db): database init

--- da-project-backend/app/__init__.py
+++ da-project-backend/app/__init__.py
@@ -1,1 +1,8 @@
 from .api import app
+from .database import create_db_and_tables
+
+
+# TODO: https://fastapi.tiangolo.com/advanced/events/
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
