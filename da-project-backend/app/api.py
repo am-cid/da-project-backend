ecdef3ef98db4e54fa9fd850889f55db82569521
am-cid <mcidantonio@gmail.com>
2024-11-16 10:59:44 +0800

    feat(api): get all and individual reports

--- da-project-backend/app/api.py
+++ da-project-backend/app/api.py
@@ -1,8 +1,33 @@
-from fastapi import FastAPI
+from typing import Annotated, List
+
+from fastapi import FastAPI, HTTPException, Query
+from sqlmodel import select
+
+from .database import SessionDep
+from .models import (
+    Report,
+    ReportResponse,
+)
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root() -> dict[str, str]:
-    return {"hello": "world"}
+@app.get("/api/report")
+def get_all_reports(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> List[ReportResponse]:
+    reports = session.exec(select(Report).offset(offset).limit(limit)).all()
+    return ReportResponse.from_reports(reports)
+
+
+@app.get("/api/report/{report_id}")
+def get_report(
+    report_id: int,
+    session: SessionDep,
+) -> ReportResponse:
+    report = session.get(Report, report_id)
+    if not report:
+        raise HTTPException(status_code=404, detail="Report not found")
+    return ReportResponse.from_report(report)
