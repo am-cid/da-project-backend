edf2ce01ca627dbdbe1c0be00049ed72e2fc3de0
am-cid <mcidantonio@gmail.com>
2024-11-17 00:29:38 +0800

    feat(api): get column data by label with optional operation done on top of it
    when operation is set, column data type must be NUMBER or else it will
    return `null`.
    
    operation = response type
    MAX = number
    MEAN = number
    MEDIAN = number
    MIN = number
    MODE = array<number>
    SUM = number

--- da-project-backend/app/api.py
+++ da-project-backend/app/api.py
@@ -1,3 +1,5 @@
+import enum
+from collections import Counter
 from typing import Annotated, List
 
 from fastapi import FastAPI, HTTPException, Query
@@ -6,8 +8,8 @@ from sqlmodel import col, select
 from .database import SessionDep
 from .models import (
     Column,
-    ColumnResponse,
     ColumnDataType,
+    ColumnResponse,
     Comment,
     CommentCreate,
     CommentResponse,
@@ -65,7 +67,7 @@ def post_page_comment(
     return CommentResponse.from_comment(db_comment)
 
 
-@app.get("/api/report-page/{page_id}/columns")
+@app.get("/api/report-page/{page_id}/column")
 def get_report_page_columns(
     page_id: int,
     session: SessionDep,
@@ -93,3 +95,80 @@ def get_report_page_columns(
         )
         for label, row_type, rows in res
     ]
+
+
+class ColumnOperation(enum.StrEnum):
+    SUM = "sum"
+    MEAN = "mean"
+    MEDIAN = "median"
+    MODE = "mode"
+    MAX = "max"
+    MIN = "min"
+
+
+@app.get(
+    "/api/report-page/{page_id}/column/{label}",
+    description="""
+If no operation is specified, this will return `array<string> | null`.
+Response type is dependent on optional operation query param.
+
+If operation is set, the row data type in the db must be `"num"` or else this
+will return `null`.
+
+    operation = response type
+    max = number
+    mean = number
+    median = number
+    min = number
+    mode = array<number>
+    sum = number
+""",
+)
+def get_report_page_column_data_by_label(
+    page_id: int,
+    label: str,
+    session: SessionDep,
+    operation: ColumnOperation | None = None,
+) -> list[str] | list[float] | float | None:
+    statement = (
+        select(Column.rows, Column.dtype)
+        .select_from(Page, Column)
+        .where(Page.page_id == page_id)
+        .where(Column.report_id == Page.report_id)
+        .where(Column.label == label)
+    )
+    res = session.exec(statement).first()
+    if not res:
+        return None
+    (row, dtype) = res
+    if not row:
+        return None
+    row_data = row.split(",")
+    if not operation:
+        return row_data
+    if dtype != ColumnDataType.NUMBER:
+        return None
+    row_data = list(map(float, row_data))
+    match operation:
+        case ColumnOperation.MAX:
+            return max(row_data)
+        case ColumnOperation.MEAN:
+            return sum(row_data) / len(row_data)
+        case ColumnOperation.MEDIAN:
+            row_data.sort()
+            n = len(row_data)
+            if n % 2 != 0:
+                return row_data[n // 2]
+            else:
+                mid_1 = row_data[(n // 2) - 1]
+                mid_2 = row_data[(n // 2)]
+                return (mid_1 + mid_2) / 2
+        case ColumnOperation.MIN:
+            return min(row_data)
+        case ColumnOperation.MODE:
+            counts = Counter(row_data)
+            max_freq = max(counts.values())
+            modes = [key for key, val in counts.items() if val == max_freq]
+            return modes
+        case ColumnOperation.SUM:
+            return sum(row_data)
