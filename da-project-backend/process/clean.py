e08914ca09612ebc277182a7a7eb881df25dfada
am-cid <mcidantonio@gmail.com>
2024-11-18 22:07:25 +0800

    feat(process): clean csv functionality. rudimentary boolean column detection.

---
+++ da-project-backend/process/clean.py
@@ -0,0 +1,62 @@
+from io import StringIO
+
+import polars as pl
+import polars.datatypes as dtype
+
+from app.field_types import ColumnDataType
+
+BOOLEAN_TRUE_VALUES = {"true", "yes", "y", "on"}
+BOOLEAN_FALSE_VALUES = {"false", "no", "n", "off"}
+
+
+def clean_csv(
+    file_contents: str,
+) -> tuple[str, list[str], list[str], list[ColumnDataType]]:
+    """Returns:
+    - cleaned csv string
+    - cleaned csv labels
+    - cleaned csv rows as comma separated string
+    - cleaned csv dtype
+    """
+    df = pl.read_csv(StringIO(file_contents))
+    labels = []
+    rows = []
+    dtypes: list[ColumnDataType] = []
+    for col in df.get_columns():
+        df = df.with_columns(
+            col.cast(dtype.String).str.to_lowercase().str.strip_chars().alias(col.name),
+        )
+        og_dtype = col.dtype
+        col = df.get_column(col.name)
+        match og_dtype:
+            case dtype.String | dtype.Categorical | dtype.Enum | dtype.Utf8:
+                string_vals = col.to_list()
+                true_count = sum(1 for x in string_vals if x in BOOLEAN_TRUE_VALUES)
+                false_count = sum(1 for x in string_vals if x in BOOLEAN_FALSE_VALUES)
+                total_count = len(string_vals)
+                possibly_bool_column = true_count + false_count > total_count * 0.8
+                if possibly_bool_column:
+                    df = df.with_columns(col.is_in(BOOLEAN_TRUE_VALUES).alias(col.name))
+                    dtypes.append(ColumnDataType.BOOLEAN)
+                else:
+                    dtypes.append(ColumnDataType.STRING)
+            case dtype.Boolean:
+                dtypes.append(ColumnDataType.BOOLEAN)
+            case (
+                dtype.Decimal
+                | dtype.Float32
+                | dtype.Float64
+                | dtype.Int8
+                | dtype.Int16
+                | dtype.Int32
+                | dtype.Int64
+                | dtype.UInt8
+                | dtype.UInt16
+                | dtype.UInt32
+                | dtype.UInt64
+            ):
+                dtypes.append(ColumnDataType.NUMBER)
+        col = df.get_column(col.name)
+        labels.append(col.name)
+        rows.append(",".join(col.cast(dtype.String).to_list()))
+    return df.write_csv(), labels, rows, dtypes
--- da-project-backend/process/read.py
+++ da-project-backend/process/read.py
@@ -5,6 +5,6 @@ import polars as pl
 
 
 def read_csv(file_contents: bytes) -> list[dict[str, Any]]:
-    "reads csv from uploaded file and returns list of rows as dicts"
+    """Reads csv from uploaded file and returns list of rows as dicts"""
     df = pl.read_csv(io.BytesIO(file_contents))
     return df.to_dicts()
