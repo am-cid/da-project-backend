f854cd0d34ed465d9a9ae6d82c6b830bbb3bce7b
am-cid <mcidantonio@gmail.com>
2024-12-05 13:46:05 +0800

    feat(process): cleaning csv now identifies whether a column is possibly a currency column

--- da-project-backend/process/clean.py
+++ da-project-backend/process/clean.py
@@ -3,7 +3,7 @@ from typing import Literal
 
 import polars as pl
 import polars.datatypes as dtype
-from app.types import ColumnDataType
+from app.types import ColumnDataType, CurrencySymbol
 
 BOOLEAN_TRUE_VALUES = {"true", "yes", "y", "on"}
 BOOLEAN_FALSE_VALUES = {"false", "no", "n", "off"}
@@ -12,12 +12,15 @@ BOOLEAN_FALSE_VALUES = {"false", "no", "n", "off"}
 def clean_csv(
     file_contents: str,
     strategy: Literal["forward", "backward", "min", "max", "mean", "zero", "one"],
-) -> tuple[str, list[str], list[str], list[ColumnDataType]]:
+) -> tuple[
+    str, list[str], list[str], list[ColumnDataType], list[CurrencySymbol | None]
+]:
     """Returns:
     - cleaned csv string
     - cleaned csv labels
     - cleaned csv rows as comma separated string
     - cleaned csv dtype
+    - cleaned csv currency symbols (if any)
     """
     df = pl.read_csv(
         StringIO(
@@ -27,6 +30,7 @@ def clean_csv(
     labels = []
     rows = []
     dtypes: list[ColumnDataType] = []
+    currencies: list[CurrencySymbol | None] = []
     for col in df.get_columns():
         col_name = col.name.strip()
         df = df.with_columns(
@@ -40,10 +44,19 @@ def clean_csv(
                 if possibly_bool_column(string_vals):
                     df = df.with_columns(col.is_in(BOOLEAN_TRUE_VALUES).alias(col_name))
                     dtypes.append(ColumnDataType.BOOLEAN)
+                    currencies.append(None)
+                elif res := possibly_currency_column(string_vals):
+                    df = df.with_columns(
+                        col.str.strip_prefix(res.value).alias(col_name)
+                    )
+                    dtypes.append(ColumnDataType.NUMBER)
+                    currencies.append(res)
                 else:
                     dtypes.append(ColumnDataType.STRING)
+                    currencies.append(None)
             case dtype.Boolean:
                 dtypes.append(ColumnDataType.BOOLEAN)
+                currencies.append(None)
             case (
                 dtype.Decimal
                 | dtype.Float32
@@ -58,10 +71,14 @@ def clean_csv(
                 | dtype.UInt64
             ):
                 dtypes.append(ColumnDataType.NUMBER)
+                currencies.append(None)
+            case _:
+                dtypes.append(ColumnDataType.STRING)
+                currencies.append(None)
         col = df.get_column(col_name)
         labels.append(col_name)
         rows.append(",".join(col.cast(dtype.String).to_list()))
-    return df.write_csv(), labels, rows, dtypes
+    return df.write_csv(), labels, rows, dtypes, currencies
 
 
 def remove_comma_inside_quotes(file_contents: str) -> str:
@@ -85,3 +102,26 @@ def possibly_bool_column(string_vals: list[str]) -> bool:
     false_count = sum(1 for x in string_vals if x in BOOLEAN_FALSE_VALUES)
     total_count = len(string_vals)
     return true_count + false_count > total_count * 0.8
+
+
+def possibly_currency_column(string_vals: list[str]) -> CurrencySymbol | None:
+    "returns currency symbol if found. None if none"
+    ret: CurrencySymbol | None = None
+    currency_symbol: CurrencySymbol | None = None
+    for val in string_vals:
+        if val in [""]:
+            continue
+        for symbol in iter(CurrencySymbol):
+            if val.startswith(symbol):
+                if currency_symbol is None:
+                    currency_symbol = symbol
+                elif currency_symbol != symbol:
+                    # many different symbols in column. do not bother trying to convert
+                    return None
+                ret = symbol
+                try:
+                    float(val[len(symbol) :])
+                except ValueError:
+                    # not a valid currency number
+                    return None
+    return ret
